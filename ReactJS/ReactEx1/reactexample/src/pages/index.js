8697933c660226ca84ab88a38e9509ad43f11831
derekcamarillo <dcamarillo9339@gmail.com>
2021-09-12 08:59:17 +0530

    12Sep2021

--- ReactJS/ReactEx1/reactexample/src/pages/index.js
+++ ReactJS/ReactEx1/reactexample/src/pages/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+let currDate = new Date();
+currDate = currDate.getHours();
+let greeting = "";
+const cssStyle = {};
+if(currDate > 1 && currDate <12)
+	{
+		greeting = "Good Morning";
+		cssStyle.color = "#00b894";
+	}
+else if(currDate >= 12 && currDate < 20)
+	{
+		greeting = "Good Afternoon";
+		cssStyle.color = "orange";
+	}
+else if(currDate >= 20 && currDate < 24)
+	{
+		greeting = "Good Night";
+		cssStyle.color = "black";
+	}
+
 const Home = () => {
 return (
 	<div>
-	<h1>Welcome to Homepage</h1>
+	<h1>Welcome to Homepage, <span style={cssStyle}> {greeting} </span></h1>
 	</div>
 );
 };
